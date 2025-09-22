3892c64813c0134e4e8458c36a21f7ddf67bb49b
Edwinkorir38 <ekorir99@gmail.com>
2025-09-23 00:50:15 +0300

    made some updates to solve conflicts

--- test/serverTest.js
+++ test/serverTest.js
@@ -1,35 +1,36 @@
-process.env.NODE_ENV = 'test';
+// Set environment to test
 process.env.NODE_ENV = 'test';
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const server = require('../server');
+const server = require('../server'); // Make sure server exports an app instance
 
 const should = chai.should();
 const expect = chai.expect;
 
 chai.use(chaiHttp);
-var should = chai.should();
 
 describe('Photos', function () {
   it('should load the home page on / GET', function (done) {
-    this.timeout(60000);
+    this.timeout(60000); // Optional: if your server takes time to start
+
     chai.request(server)
       .get('/')
       .end(function (err, res) {
         if (err) return done(err);
 
-        res.should.have.status(200);  // Expect HTTP 200
-        res.should.be.html;           // Expect HTML response
+        res.should.have.status(200);       // Expect HTTP 200 OK
+        res.should.be.html;                // Expect HTML response
 
-        // Match actual HTML content
+        // Check specific content in the page
         expect(res.text).to.include('MILESTONE 2');
         expect(res.text).to.include('Image Upload');
 
-        // Check that the images container exists
+        // Ensure the div with class "row" exists
         expect(res.text).to.match(/<div class="row">/);
 
         done();
       });
+  });
 });
-});
+
