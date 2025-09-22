47a447814927841becb60019501e1420e06102e6
Edwinkorir38 <ekorir99@gmail.com>
2025-09-22 12:07:31 +0300

    modified _config.js

--- _config.js
+++ _config.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 var config = {};
 
 config.mongoURI = {
-    production: process.env.MONGO_URI_PRODUCTION || 'mongodb+srv://Edwinkorir38:Hakuna123@cluster0.qzalcrk.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: process.env.MONGO_URI_DEVELOPMENT || 'mongodb+srv://Edwinkorir38:Hakuna123@cluster0.qzalcrk.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: process.env.MONGO_URI_TEST || 'mongodb+srv://Edwinkorir38:Hakuna123@cluster0.qzalcrk.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: process.env.MONGO_URI_PRODUCTION,
+    development: process.env.MONGO_URI_DEVELOPMENT,
+    test: process.env.MONGO_URI_TEST,
 };
 
 config.port = process.env.PORT || 5000;
