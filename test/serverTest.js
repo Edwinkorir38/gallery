958b45d4a55444c7f3c7871e0c6c22bca97b708c
Edwinkorir38 <ekorir99@gmail.com>
2025-09-22 13:26:52 +0300

    updated some changes

--- test/serverTest.js
+++ test/serverTest.js
@@ -1,26 +1,21 @@
-process.env.NODE_ENV = 'test';   
+process.env.NODE_ENV = 'test';
 
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-
 var server = require('../server');
-var should = chai.should();
-var expect = chai.expect;
 
 chai.use(chaiHttp);
+var should = chai.should();
 
-describe('Photos', function(){
-
-
-    it('should list ALL photos on / GET', function(done){
-        this.timeout(60000);
-        chai.request(server)
-        .get('/')
-        .end(function(err,res){
-            res.should.have.status(200);
-            res.should.be.html;
-            res.body.should.be.a('object')
-            done();
-        })
-    });
-})
+describe('Health Check', function() {
+  it('should return ok on /health GET', function(done) {
+    chai.request(server)
+      .get('/health')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.should.have.property('status').eql('ok');
+        done();
+      });
+  });
+});
